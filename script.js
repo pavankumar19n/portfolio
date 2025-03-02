29ba13ba007e21eb0be0616707fd6de0cd42030e
pavan kumar <165786365+pavankumar19n@users.noreply.github.com>
2025-03-02 12:15:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu(){
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
+
+// const image1 = document.querySelector('.icon-1');
+// const image2 = document.querySelector('.icon-2');
+// image1.addEventListener('click', function()){
+
+// }
